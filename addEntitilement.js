d59f26735d56b98e6a45a58540bc2e934781695c
CaoDuc <caonhatduc9@gmail.com>
2023-08-20 04:42:17 +0700

    clean

--- addEntitilement.js
+++ addEntitilement.js
@@ -91,79 +91,15 @@ async function executeTest() {
       By.id("entitlements_employee_empName")
     );
     inputElement.click();
-    // await inputElement.sendKeys(Key.chord(Key.CONTROL, "a"), Key.DELETE);
-    await driver.sleep(1000);
-    await inputElement.sendKeys(Key.chord(Key.CONTROL, "a"), Key.DELETE);
+    await inputElement.clear();
 
     await inputElement.sendKeys(values[0]);
 
     await inputElement.sendKeys(Key.ENTER);
-    // await driver.sleep(1000);
-    // let selectElement = await driver.findElement(
-    //   By.id("entitlements_leave_type")
-    // );
     let inputEntiti = await driver
       .findElement(By.id("entitlements_entitlement"))
       .sendKeys(values[3]);
-    await driver.sleep(1000);
-    await driver.findElement(By.id("btnSave")).click();
-
-    // await driver.sleep(1000);
-
-    // await driver
-    //   .findElement(By.id("entitlements_employee_empName"))
-    //   .sendKeys(Key.DELETE);
-    // await driver.findElement(By.id("employeeId")).sendKeys(values[0]);
-
-    // await driver.findElement(By.css("fieldset > ol > li:nth-child(2)")).click();
-    // await driver.findElement(By.id("entitlements_employee_empName")).click();
-    // await driver.findElement(By.css(".ac_even")).click();
-
-    // await driver.sleep(1000);
-
-    // return 0;
-    // await driver.findElement(By.name("firstName")).sendKeys(values[1]);
-
-    // await driver.findElement(By.name("middleName")).sendKeys(values[2]);
-
-    // await driver.findElement(By.name("lastName")).sendKeys(values[3]);
-
-    // await driver
-    //   .findElement(By.id("employeeId"))
-    //   .sendKeys(Key.chord(Key.CONTROL, "a"), Key.DELETE);
-    // await driver.findElement(By.id("employeeId")).sendKeys(values[0]);
-
-    // await driver.sleep(100);
-    // console.log("Waiting for");
-
-    // const checkbox = await driver.findElement(By.id("chkLogin"));
-    // const isSelected = await checkbox.isSelected();
-    // if (!isSelected) await driver.findElement(By.id("chkLogin")).click();
-    // console.log("Waiting for1");
-
-    // await driver.findElement(By.id("user_name")).sendKeys(values[4]);
-    // console.log("Waiting for2");
-    // await driver.findElement(By.id("user_password")).sendKeys(values[5]);
-    // await driver.findElement(By.id("re_password")).click();
-    // await driver.findElement(By.id("re_password")).sendKeys(values[6]);
-    // await driver.sleep(100);
-
-    // await driver.findElement(By.id("btnSave")).click();
-    // await driver.sleep(1200);
-    // console.log("check bẻoe");
-
-    // try {
-    //   let isSuccess = await waitForElement("#addEmployeeTbl > div");
-    //   console.log("check success", isSuccess);
-    //   if (isSuccess) {
-    //     checkAlreadyExists = true;
-    //     let text = await isSuccess.getText();
-    //     console.log("isSuccess:", text);
-    //     result.push(text);
-    //   }
-    // } catch (error) {
-    //   console.log("Phần tử không tồn tại:", error.message);
-    // }
+    await driver.sleep(500);
 
     let employeeMessage = await driver.findElements(
       By.css("#frmLeaveEntitlementAdd > fieldset > ol > li:nth-child(2) > span")
@@ -194,91 +130,6 @@ async function executeTest() {
       result.push("Successfully Saved");
     }
 
-    // let lastnameMessage = await driver.findElements(
-    //   By.css(
-    //     "#frmAddEmp > fieldset > ol > li.line.nameContainer > ol > li:nth-child(3) > span"
-    //   )
-    // );
-
-    // if (lastnameMessage.length > 0 && lastnameMessage.length == 0) {
-    //   isPass = false;
-    //   for (let i = 0; i < lastnameMessage.length; i++) {
-    //     let text = await lastnameMessage[i].getText();
-    //     console.log("lastnameMessage", i, ":", text);
-    //     result.push("Input LastName: " + text);
-    //   }
-    // }
-
-    // let usernameMessage = await driver.findElements(
-    //   By.css("#frmAddEmp > fieldset > ol > li:nth-child(5) > span")
-    // );
-
-    // if (usernameMessage.length > 0) {
-    //   isPass = false;
-    //   for (let i = 0; i < usernameMessage.length; i++) {
-    //     let text = await usernameMessage[i].getText();
-    //     console.log("userName", i, ":" + text);
-    //     result.push("Input username:" + text);
-    //   }
-    // }
-
-    // let passwordMessage = await driver.findElements(
-    //   By.css("#frmAddEmp > fieldset > ol > li:nth-child(6) > span")
-    // );
-
-    // if (passwordMessage.length > 0) {
-    //   isPass = false;
-    //   for (let i = 0; i < passwordMessage.length; i++) {
-    //     let text = await passwordMessage[i].getText();
-    //     console.log("passwordMessage", i, ":", text);
-    //     result.push("Input password: " + text);
-    //   }
-    // }
-
-    // let confirmPasswordMessage = await driver.findElements(
-    //   By.css("#frmAddEmp > fieldset > ol > li:nth-child(7) > span")
-    // );
-
-    // if (confirmPasswordMessage.length > 0) {
-    //   isPass = false;
-    //   for (let i = 0; i < confirmPasswordMessage.length; i++) {
-    //     let text = await confirmPasswordMessage[i].getText();
-    //     console.log("confirmPasswordMessage", i, ":", text);
-    //     result.push("Input confirmPassword: " + text);
-    //   }
-    // }
-
-    // // await driver.sleep(1500);
-    // await driver.findElement(By.css("#menu_pim_viewPimModule > b")).click();
-    // // Thực hiện thêm nhân viên
-    // await driver.findElement(By.id("menu_pim_addEmployee")).click();
-    // await driver.findElement(By.linkText("Employee List")).click();
-    // // await driver.sleep(1500);
-    // if (checkAlreadyExists === false) {
-    //   const table = await waitForElement("#resultTable");
-    //   if (table) {
-    //     const rows = await table.findElements(By.css("tbody tr"));
-    //     console.log("row", rows);
-    //     // Số cột mà bạn muốn lấy giá trị (ví dụ: cột thứ 2)
-    //     const columnIndex = 0; // (đánh số cột từ 0)
-    //     let count = 0;
-    //     for (let i = 0; i < rows.length; i++) {
-    //       const cells = await rows[i].findElements(By.css("td"));
-
-    //       const cellTextElement = await cells[1].findElement(By.css("a"));
-    //       const cellText = await cellTextElement.getAttribute("innerText");
-    //       console.log("Giá trị ô:", cellText);
-    //       if (cellText === values[0]) {
-    //         count++;
-    //         result.push("Successfully Saved");
-    //         console.log("result finally", result);
-    //         break;
-    //       }
-    //     }
-    //   }
-    // }
-    // checkAlreadyExists = false;
-    // console.log("check exsif for=" + values[0] + "==" + checkAlreadyExists);
     index++;
     const resultString = result.join(";");
     resultFile.write(resultString + "\n");
